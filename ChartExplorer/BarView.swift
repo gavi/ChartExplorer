95b2212b45084ddeae9c4908225c6d6644f6c796
Gavi Narra <gavi.narra@objectgraph.com>
2022-06-11 11:52:06 -0400

    Initial commit

---
+++ ChartExplorer/BarView.swift
@@ -0,0 +1,31 @@
+//
+//  BarView.swift
+//  ChartExplorer
+//
+//  Created by Gavi Narra on 6/11/22.
+//
+
+import SwiftUI
+import Charts
+
+struct BarView:View{
+    let data = [("New York",900),("Pittsburgh",600), ("Chicago",1200)]
+    var body: some View{
+        VStack{
+            Text("Bar Chart").bold()
+            Chart(data,id:\.self.0){ item in
+                BarMark(
+                    x:.value("City", item.0),
+                    y:.value("Sales", item.1)
+                    
+                )
+            }
+        }
+    }
+}
+
+struct BarView_Previews:PreviewProvider{
+    static var previews: some View{
+        BarView()
+    }
+}
