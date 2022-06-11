c785fae21ac5971e5038df7e797173e1fe883352
Gavi Narra <gavi.narra@objectgraph.com>
2022-06-11 11:34:10 -0400

    Initial Commit

---
+++ ChartExplorer/ChartExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartExplorerApp.swift
+//  ChartExplorer
+//
+//  Created by Gavi Narra on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChartExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartExplorer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ChartExplorer
+//
+//  Created by Gavi Narra on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
